cab52ee27aa43be212ca0b551456fe6134bd9cad
Jeremy Boles <jeremy@jeremyboles.com>
2010-02-01 17:23:31 -0600

    Okay, its working now.

--- extension.driver.php
+++ extension.driver.php
@@ -1,5 +1,7 @@
 <?php
 
+require(dirname(__FILE__) . '/csspp/csspp.php');
+
 Class extension_csspp extends Extension
 {
   public $workspace_position = NULL;
@@ -30,6 +32,14 @@ Class extension_csspp extends Extension
   
   public function replace_css(&$context)
   {
-    $context['output'] = preg_replace_callback('/^media\/css\/(.*\.css)$/', array(&$this, 'replace_css'), $context['output']);
+    // <link type="text/css" rel="stylesheet" media="screen" href="/media/css/baseline.reset.css"/>
+    preg_match_all('#<link\s[^>]*href=\"\/workspace\/media\/css\/([^\"]*).css\"[^>]*>#', $context['output'], $found);
+    foreach ($found[1] as $i => $file)
+    {
+      $csspp = new CSSPP("$file.css", DOCROOT . '/workspace/media/css/');
+      file_put_contents(DOCROOT . "/workspace/media/css/$file-processed.css" , $csspp->process());
+      $new = str_replace($file, "$file-processed", $found[0][$i]);
+      $context['output'] = str_replace($found[0][$i], $new, $context['output']);
+    }
   }
 }
